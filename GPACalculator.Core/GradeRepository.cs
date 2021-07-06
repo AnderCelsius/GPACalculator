581d1fdefc8947cd0749538b7b47dd7c8d9a5fae
obinna asiegbulam <oasiegbulam@gmail.com>
2021-07-06 09:11:06 +0100

    Add Score method Create GradeTable List

--- GPACalculator.Core/GradeRepository.cs
+++ GPACalculator.Core/GradeRepository.cs
@@ -1,8 +1,79 @@
-﻿using System;
+﻿using GPACalculator.Data;
+using GPACalculator.Model;
+using System;
 
 namespace GPACalculator.Core
 {
     public class GradeRepository
     {
+        int count = 1;
+
+        public string AddScore(string course, int courseUnit, int score)
+        {
+            var message = string.Empty;
+            int previousItemCount = DataStore.GradeTable.Count;
+
+            var courseCode = course.Insert(3, " ");
+            var grade = "";
+            int gradePoint;
+            int weightPoint = 0;
+            var remark = "";
+
+            if (score >= 70)
+            {
+                gradePoint = (int)GradeModel.GradeSystem.A;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Excellent";
+            }
+            else if (score >= 60)
+            {
+                gradePoint = (int)GradeModel.GradeSystem.B;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Very Good";
+            }
+            else if (score >= 50)
+            {
+                gradePoint = (int)GradeModel.GradeSystem.C;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Good";
+            }
+            else if (score >= 45)
+            {
+                gradePoint = (int)GradeModel.GradeSystem.D;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Fair";
+            }
+            else if (score >= 40)
+            {
+                gradePoint = (int)GradeModel.GradeSystem.E;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Pass";
+            }
+            else
+            {
+                gradePoint = (int)GradeModel.GradeSystem.F;
+                grade += (GradeModel.GradeSystem)gradePoint;
+                weightPoint += courseUnit * gradePoint;
+                remark += "Fail";
+            }
+
+            var record = new GradeModel(count, courseCode, courseUnit, score, grade, gradePoint, weightPoint, remark);
+            DataStore.GradeTable.Add(record);
+            int updatedItemCount = DataStore.GradeTable.Count;
+            count++;
+
+            if (updatedItemCount > previousItemCount)
+            {
+                message = "Course added succesfully!";
+            }
+
+
+            return message;
+        }
     }
 }
--- GPACalculator.Data/DataStore.cs
+++ GPACalculator.Data/DataStore.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using GPACalculator.Model;
+using System;
+using System.Collections.Generic;
 
 namespace GPACalculator.Data
 {
     public class DataStore
     {
+        public static List<GradeModel> GradeTable { get; set; } = new List<GradeModel>();
     }
 }
